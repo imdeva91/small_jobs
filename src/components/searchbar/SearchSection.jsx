5698517026a2bbd435472e3b98bccf4bd97c61da
Aniket <aniketsheriyarji@gmail.com>
2024-03-30 17:12:56 +0530

    home page

---
+++ src/components/searchbar/SearchSection.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const SearchSection = () => {
+    return(
+        <>
+            <div className=' p-6 max-w-100 w-full rounded-2xl shadow-black shadow-2xl'>
+
+                <div className=' block'>
+                    <h1>Search Worker</h1>
+                    <form className='' action="search">
+
+                        <div className=' flex justify-evenly'>
+                            <div>
+                                <label htmlFor="job-title">Select job title</label>
+                                <input className=' border-solid border-2 border-sky-500 rounded-md ' type="search" id='search' name='search' />
+                                <input type="submit" value='submit' />
+                            </div>
+                            <div>
+                                <label htmlFor="job-title">Select job title</label>
+                                <input className=' border-solid border-2 border-sky-500 rounded-md ' type="search" id='search' name='search' />
+                                <input type="submit" value='submit' />
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        
+        
+        </>
+    )
+}
+
+export default SearchSection;
