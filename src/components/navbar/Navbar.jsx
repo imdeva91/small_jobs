122df89ecf875053fc49836f34af6b1e8988d4a5
imdeva91 <imdeva91@gmail.com>
2024-03-30 15:06:45 +0530

    navbar-changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,13 +3,30 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <header className="shadow-xl">
-        <div className="max-w-[1200px] mx-auto flex items-center">
+      <header className="shadow-xl w-full h-16">
+        <div className="max-w-[1200px] flex items-center justify-around">
           <div>Logo</div>
-          <div>
-            <li></li>
-            <li></li>
-            <li></li>
+          <div className="flex list-none gap-10 items-center ">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact </li>
+          </div>
+          <div className="flex gap-10 items-center">
+            <div>
+              <button className="bg-indigo-700 px-5 py-2 text-white rounded-lg">
+                Hire Now
+              </button>
+            </div>
+            <div>
+              <button>Signin</button>
+            </div>
+            <div className="h-[50px] w-[50px] rounded-full bg-red-500 overflow-hidden">
+              <img
+                className="object-cover h-[50px] w-[50px]"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="img"
+              />
+            </div>
           </div>
         </div>
       </header>
