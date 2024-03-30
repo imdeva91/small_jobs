0e607141926ea85098aa064d9b070c0263837622
imdeva91 <imdeva91@gmail.com>
2024-03-30 14:38:25 +0530

    second commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <header className="shadow-xl">
+        <div className="max-w-[1200px] mx-auto flex items-center">
+          <div>Logo</div>
+          <div>
+            <li></li>
+            <li></li>
+            <li></li>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
